90a5fb1fdc4a5e735bfadb24c0e62c6bafe7f4c9
Otabek <32852000+Otabek-KBTU@users.noreply.github.com>
2017-12-05 03:01:33 +0600

    Add files via upload

---
+++ 2nd attestation/sochet.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+	using namespace std;
+
+	const int MAX = 2e5 + 10;
+
+	int n, k;
+	int a[MAX];
+	void comb(int x, int last) {
+		if(x == k) {
+			for(int i = 0; i < k; i++) {
+				cout << a[i] <<" "<<"\n"<<a[i == k - 1];
+			}
+
+		}	else  {
+			for(int i = last + 1; i <= n; i++) {
+				a[x] = i;
+				comb(x + 1, i);
+			}
+		}
+	}
+
+	int main() {
+		cin >> n >> k;
+		comb(0, 0);
+		return 0;
+	}
